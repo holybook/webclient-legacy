038b9186ef90fa60adca7341bec5b734e991ca22
Hannes Widmoser <widmoser@gmail.com>
2014-11-27 22:55:39 +0100

    added a navigation element for books

--- src/app/reader/book.reader.controller.js
+++ src/app/reader/book.reader.controller.js
@@ -3,12 +3,34 @@
 angular.module('holybook').controller('BookReader',
     function ($scope, $stateParams, api) {
 
-        $scope.result = api.book.get({
-           language : $stateParams.language,
-            religion : $stateParams.religion,
-            author : $stateParams.author,
-            title : $stateParams.title
-        });
+        var paragraphsPerPage = 50;
+        $scope.pagination = {
+            current: 1
+        };
+
+        function getParagraphs(page) {
+            api.book.get({
+                language: $stateParams.language,
+                religion: $stateParams.religion,
+                author: $stateParams.author,
+                title: $stateParams.title,
+                from: (page - 1)*paragraphsPerPage,
+                size: paragraphsPerPage
+            }, function (data) {
+                $scope.book = data.book;
+                $scope.pagination.current = page;
+                $scope.pagination.total = Math.ceil(data.paragraphs / paragraphsPerPage);
+                $scope.hasResult = true;
+            });
+        }
+
+        $scope.pageChanged = function(newPage) {
+            if (newPage !== $scope.pagination.current) {
+                getParagraphs(newPage);
+            }
+        };
+
+        getParagraphs(1);
 
     }
 );
