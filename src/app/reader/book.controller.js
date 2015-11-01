659f3cb339c8bfd7c814b02008c5c5cc07692b50
Hannes Widmoser <widmoser@gmail.com>
2015-11-01 22:19:26 +0100

    fixed book reader

--- src/app/reader/book.controller.js
+++ src/app/reader/book.controller.js
@@ -7,48 +7,24 @@ angular.module('holybook').controller('Book',
 
         var paragraphsPerPage = 25;
 
+        BookCtrl.pagination = {
+            current: 1,
+            total: 0
+        };
+
         utils.connect($scope, 'BookCtrl.pagination.current', 'page', function(page) {
-            getParagraphs(page);
+            if (page !== undefined && !isNaN(page)) {
+                getParagraphs(page);
+            }
         }, parseInt);
 
         api.book.get({
             language: "en",
             id: $stateParams.id
         }, function (book) {
-
-            for (var i in book.sections) {
-                if (i > 0) {
-                    book.sections[i - 1].end = book.sections[i].start;
-                }
-            }
-            book.sections[book.sections.length - 1].end = book.paragraphs;
-
             BookCtrl.book = book;
             BookCtrl.pagination.total = Math.ceil(book.paragraphs / paragraphsPerPage);
         });
-
-        function insertText(text) {
-            var startIndex = (BookCtrl.pagination.current - 1)*paragraphsPerPage;
-            var endIndex = startIndex + paragraphsPerPage;
-            BookCtrl.visibleSections = Lazy(BookCtrl.book.sections).reduce(function(agg, section) {
-                if (section.start <= startIndex && section.end > startIndex) {
-                    agg.push({
-                        'start' : section.start,
-                        'offset' : startIndex - section.start,
-                        'text' : text.slice(0, Math.min(section.end, endIndex) - startIndex),
-                        'title' : section.title
-                    });
-                } else if (section.start >= startIndex && section.start < endIndex) {
-                    agg.push({
-                        'start' : section.start,
-                       'offset' : 0,
-                        'text' : text.slice(section.start - startIndex, Math.min(section.end, endIndex) - startIndex),
-                        'title' : section.title
-                    });
-                }
-                return agg;
-            }, []);
-        }
 
         function getParagraphs(page) {
             api.text({
@@ -56,11 +32,8 @@ angular.module('holybook').controller('Book',
                 id: $stateParams.id,
                 from: (page - 1)*paragraphsPerPage,
                 size: paragraphsPerPage
-            }).success(function (text) {
-                BookCtrl.hasResult = true;
-
-                insertText(text);
-
+            }).success(function (sections) {
+                BookCtrl.sections = sections;
                 if (typeof($stateParams.select) !== 'undefined') {
                     BookCtrl.selected = $stateParams.select;
                 }
@@ -94,7 +67,7 @@ angular.module('holybook').controller('Book',
             return BookCtrl.isSelected(section, $index) ? 'book-paragraph-selected' : '';
         };
 
-        return $scope.BookReaderCtrl = BookCtrl;
+        return $scope.BookCtrl = BookCtrl;
 
     }
 );
